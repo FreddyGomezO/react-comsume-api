052f0e4de4ce7cd35a2c311834d1b4926a6b50dc
FreddyGomezO <freddy.gomez.est@tecazuay.edu.ec>
2025-07-04 08:31:44 -0500

    Describe aquí qué cambiaste

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,22 +1,27 @@
-import axios from "axios";
-
 export class ProductService {
-
     baseUrl = "https://freddy-production-api-5977f618ffa6.herokuapp.com/api/products";
 
-    create(product){
-        return axios.post(this.baseUrl + "/product", product).then(res => res.data);
+    create(product) {
+        return axios.post(this.baseUrl, product, { 
+            withCredentials: true 
+        }).then(res => res.data);
     }
 
-    readAll(){
-        return axios.get(this.baseUrl).then(res => res.data);
+    readAll() {
+        return axios.get(this.baseUrl, { 
+            withCredentials: true 
+        }).then(res => res.data);
     }
 
-    update(product){
-        return axios.put(this.baseUrl + "/product/" + product._id, product).then(res => res.data);
+    update(product) {
+        return axios.put(`${this.baseUrl}/${product._id}`, product, { 
+            withCredentials: true 
+        }).then(res => res.data);
     }
 
-    delete(id){
-        return axios.delete(this.baseUrl + "/product/" + id).then(res => res.data);
+    delete(id) {
+        return axios.delete(`${this.baseUrl}/${id}`, { 
+            withCredentials: true 
+        }).then(res => res.data);
     }
 }
